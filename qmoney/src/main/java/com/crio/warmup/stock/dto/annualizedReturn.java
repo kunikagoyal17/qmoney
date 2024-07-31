513595c6a8d0522da1dd87ee8863270c4c0e9362
kunikagoyal97@gmail.com <your.email@example.com>
2024-07-31 19:05:12 +0530

    Your commit message

--- qmoney/src/main/java/com/crio/warmup/stock/dto/annualizedReturn.java
+++ qmoney/src/main/java/com/crio/warmup/stock/dto/annualizedReturn.java
@@ -1,5 +1,36 @@
+
 package com.crio.warmup.stock.dto;
 
-public class annualizedReturn {
+//import java.time.LocalDate;
 
+public class AnnualizedReturn {
+
+  private final String symbol;
+  private final Double annualizedReturn;
+  private final Double totalReturns;
+
+  public AnnualizedReturn(String symbol, Double annualizedReturn, Double totalReturns) {
+    this.symbol = symbol;
+    this.annualizedReturn = annualizedReturn;
+    this.totalReturns = totalReturns;
+  }
+
+ // public  AnnualizedReturn(LocalDate localDate, PortfolioTrade[] trades, Double openingprice,
+     // Double closingprice) {
+       //  return null ;
+       // }
+         
+
+//public String getSymbol() {
+  public String getSymbol() {
+    return symbol;
+  }
+
+  public Double getAnnualizedReturn() {
+    return annualizedReturn;
+  }
+
+  public Double getTotalReturns() {
+    return totalReturns;
+  }
 }
